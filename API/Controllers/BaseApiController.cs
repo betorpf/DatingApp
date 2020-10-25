469023f5ed103019123e891486ee23c455da11a6
betorpf <35878076+betorpf@users.noreply.github.com>
2020-10-25 19:18:52 -0300

    End of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        // private readonly DataContext _context;
+        // public BaseApiController (DataContext context) {
+        //     this._context = context;
+        // }
+
+
+    }
+}
